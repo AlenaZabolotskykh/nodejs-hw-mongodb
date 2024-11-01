e71b4d1ff81ccf8883199c34b41366c4438f74ee
Alona Zabolotskykh <alenka.polishchuk@gmail.com>
2024-11-01 14:49:03 +0200

    fix

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -13,7 +13,7 @@ export const updateContact = async ({ _id, payload, options = {} }) => {
     new: true,
     includeResultMetadata: true,
   });
-  if (rawResult || !rawResult.value) return null;
+  if (!rawResult || !rawResult.value) return null;
   return {
     data: rawResult.value,
     isNew: Boolean(rawResult.lastErrorObject.upserted),
