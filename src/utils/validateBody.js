7be3066d237676394c5e8fb22699bfeda058913a
Alona Zabolotskykh <alenka.polishchuk@gmail.com>
2024-11-12 17:35:25 +0200

    fix validation

--- src/utils/validateBody.js
+++ src/utils/validateBody.js
@@ -3,7 +3,7 @@ import createHttpError from 'http-errors';
 const validateBody = (schema) => {
   const func = async (req, res, next) => {
     try {
-      await schema.validate(req.body, { abortEarly: false });
+      await schema.validateAsync(req.body, { abortEarly: false });
       next();
     } catch (error) {
       return next(createHttpError(404, error.message));
