d6ec122564323e62bb4e9d518a091d85a144d519
Alona Zabolotskykh <alenka.polishchuk@gmail.com>
2024-11-14 10:42:48 +0200

    session

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -9,3 +9,12 @@ export const registerController = async (req, res) => {
     message: 'Successfully registered user',
   });
 };
+
+export const loginController = async (req, res) => {
+  const session = await authServices.login(req.body);
+
+  //   res.status(201).json({
+  //     status: 201,
+  //     message: 'Successfully registered user',
+  //   });
+};
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,13 +1,28 @@
 import createHttpError from 'http-errors';
+import bcrypt from 'bcrypt';
 
 import UserCollection from '../db/models/User.js';
 
 export const register = async (payload) => {
-  const { email } = payload;
+  const { email, password } = payload;
   const user = await UserCollection.findOne({ email });
   if (user) {
     throw createHttpError(409, 'Email already in use');
   }
 
-  return UserCollection.create(payload);
+  const hashPassword = await bcrypt.hash(password, 10);
+
+  return UserCollection.create({ ...payload, password: hashPassword });
+};
+
+export const login = async ({ email, password }) => {
+  const user = await UserCollection.findOne({ email });
+  if (!user) {
+    throw createHttpError(401, 'Email or password invalid');
+  }
+
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  if (!passwordCompare) {
+    throw createHttpError(401, 'Email or password invalid');
+  }
 };
